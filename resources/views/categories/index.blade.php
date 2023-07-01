263190902abe244c2dcf16a1b3f771f5dbab52ae
Haruto9981 <aketaharuto46@icloud.com>
2023-07-01 14:02:44 +0900

    eighth commit

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -12,7 +12,6 @@
             @foreach ($posts as $post)
                 <div class='post'>
                     <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
-                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
                     <p class='body'>{{ $post->body }}</p>
                     <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
                         @csrf
@@ -26,6 +25,7 @@
             {{ $posts->links() }}
         </div>
         <a href='/posts/create'>create</a>
+        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
         
         <script>
             function deletePost(id) {
