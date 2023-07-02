76a356261fd8a936e2f490bba67ec356b7868ccf
Haruto9981 <aketaharuto46@icloud.com>
2023-07-02 23:44:32 +0900

    nineth commit

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,22 +7,24 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
+    <x-app-layout>
+        <body>
+            <h1 class="title">
+                {{ $post->title }}
+            </h1>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            <div class="content">
+                <div class="content__post">
+                    <h3>本文</h3>
+                    <p>{{ $post->body }}</p>    
+                </div>
             </div>
-        </div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-        <div class="edit">
-            <a href="/posts/{{ $post->id }}/edit">edit</a>
-        </div>
-    </body>
+            <div class="footer">
+                <a href="/">戻る</a>
+            </div>
+            <div class="edit">
+                <a href="/posts/{{ $post->id }}/edit">edit</a>
+            </div>
+        </body>
+    </x-app-layout>   
 </html>
