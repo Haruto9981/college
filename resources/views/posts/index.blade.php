f4e3751c03876a662d1dee5470399a9f473722b1
Haruto9981 <aketaharuto46@icloud.com>
2023-07-03 00:33:51 +0900

    tenth commit

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,6 +30,16 @@
             
             <h1>ログインユーザー：{{ Auth::user()->name }}</h1>
             
+            <div>
+                @foreach($questions as $question)
+                    <div>
+                        <a href="https://teratail.com/questions/{{ $question['id'] }}">
+                            {{ $question['title'] }}
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+                    
             <script>
                 function deletePost(id) {
                 'use strict'
